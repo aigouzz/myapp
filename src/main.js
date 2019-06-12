f472096047564d2427bcef10483487f1328e64a7
guoxunchao <guoxunchao@xiaomi.com>
2019-06-12 20:03:13 +0800

    修改project

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import Vue from 'vue/dist/vue.min'
+import App from './App';
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  components: {
+    App
+  },
+  template: '<App />'
+});
